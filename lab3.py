48caf112fe5cb1796ef2429d9f09c9c80a8086c5
Krzysztof <kczech123kczech123@gmail.com>
2022-10-27 13:08:55 +0200

    lab3

--- lab3.py
+++ lab3.py
@@ -5,33 +5,33 @@ import sys
 from seaborn import heatmap
 import matplotlib.pyplot as plt
 
-def getSlices(rank, numberOfCores, gridLegth):
+def getParts(rank, numberOfCores, gridLegth):
 
     lenthPerCore = 1.0 * gridLegth / numberOfCores
     indexes = (int(lenthPerCore* rank), gridLegth -1 if rank == numberOfCores else int(lenthPerCore * (rank+1)-1))
     return indexes
 
-def communicate(comm, rank, numberOfCores, gridLegth, matrix, upSlice, downSlice, begin, end):
+def communicate(comm, rank, numberOfCores, gridLegth, matrix, upPart, downPart, begin, end):
     if rank % 2 == 0:
         if rank > 0:
             comm.Send([matrix[0], gridLegth + 2, MPI.DOUBLE], dest=rank - 1)
         if rank < numberOfCores - 1:
-            comm.Recv(upSlice, source=rank + 1)
+            comm.Recv(upPart, source=rank + 1)
         if rank < numberOfCores - 1:
             comm.Send([matrix[end - begin], gridLegth + 2, MPI.DOUBLE], dest=rank + 1)
         if rank > 0:
-            comm.Recv(downSlice, source=rank - 1)
+            comm.Recv(downPart, source=rank - 1)
     else:
         if rank < numberOfCores - 1:
-            comm.Recv(upSlice, source=rank + 1)
+            comm.Recv(upPart, source=rank + 1)
         if rank > 0:
             comm.Send([matrix[0], gridLegth + 2, MPI.DOUBLE], dest=rank - 1)
         if rank > 0:
-            comm.Recv(downSlice, source=rank - 1)
+            comm.Recv(downPart, source=rank - 1)
         if rank < numberOfCores - 1:
             comm.Send([matrix[end - begin], gridLegth + 2, MPI.DOUBLE], dest=rank + 1)
 
-def calculate(gridLegth, matrix, matrixTmp, upSlice, downSlice, begin, end):
+def calculate(gridLegth, matrix, matrixTmp, upPart, downPart, begin, end):
     for y in range(begin, end + 1):
         for x in range(1, gridLegth + 1):
             if y - 1 == 0:
@@ -39,43 +39,43 @@ def calculate(gridLegth, matrix, matrixTmp, upSlice, downSlice, begin, end):
             elif y - 1 >= begin:
                 newDown = matrix[y - 1 - begin][x]
             else:
-                newDown = downSlice[x]
+                newDown = downPart[x]
             if y + 1 == gridLegth + 1:
                 newUp = 0
             elif y + 1 <= end:
                 newUp = matrix[y + 1 - begin][x]
             else:
-                newUp = upSlice[x]
+                newUp = upPart[x]
             matrixTmp[y - begin][x] = (matrix[y - begin][x - 1] + matrix[y - begin][x + 1] + newUp + newDown) / 4
     return np.copy(matrixTmp)
 
 
-def updateMatrix(comm, rank, numberOfCores, gridLegth, matrix, matrixTmp, upSlice, downSlice, begin, end):
+def updateMatrix(comm, rank, numberOfCores, gridLegth, matrix, matrixTmp, upPart, downPart, begin, end):
     
-    communicate(comm, rank, numberOfCores, gridLegth, matrix, upSlice, downSlice, begin, end)    
-    matrix = calculate(gridLegth, matrix, matrixTmp, upSlice, downSlice, begin, end)
+    communicate(comm, rank, numberOfCores, gridLegth, matrix, upPart, downPart, begin, end)    
+    matrix = calculate(gridLegth, matrix, matrixTmp, upPart, downPart, begin, end)
     return matrix
 
 
-def joinCalculations(comm, rank, numberOfCores, gridLegth, matrix, sliceLenth, result):
+def getResult(comm, rank, numberOfCores, gridLegth, matrix, PartLenth, result):
     if rank != 0:
-        comm.Send([matrix, sliceLenth * (gridLegth + 2), MPI.DOUBLE], dest=0)
+        comm.Send([matrix, PartLenth * (gridLegth + 2), MPI.DOUBLE], dest=0)
     else:
         index = 0
 
         for row in matrix:
-            if row[1] == 0:
-                break
+            # if row[1] == 0:
+            #     break
             result[index] = row
             index += 1
 
         for i in range(numberOfCores - 1):
-            temp = np.zeros((sliceLenth, gridLegth + 2), dtype=np.float64)
+            temp = np.zeros((PartLenth, gridLegth + 2), dtype=np.float64)
             comm.Recv(temp, source=i + 1)
 
             for x in temp:
-                if x[1] == 0:
-                    break
+                # if x[1] == 0:
+                #     break
 
                 result[index] = x
                 index += 1
@@ -90,7 +90,7 @@ def main():
     conductorVoltage =  int(sys.argv[2])
     conductorSize =  int(sys.argv[3])
     iterations = 100
-    sliceLenth = int(gridLegth / numberOfCores ) 
+    PartLenth = int(gridLegth / numberOfCores ) 
 
     if rank == 0:
         times = 0
@@ -100,8 +100,8 @@ def main():
         if rank == 0:
             start = time.time()
 
-        begin, end = getSlices(rank, numberOfCores, gridLegth)
-        matrix = np.random.rand(sliceLenth, gridLegth + 2)
+        begin, end = getParts(rank, numberOfCores, gridLegth)
+        matrix = np.random.rand(PartLenth, gridLegth + 2)
 
         for y in range(end - begin + 1):
             matrix[y][0] = 0
@@ -115,19 +115,19 @@ def main():
             for y in range(min(conductorMax-begin, end-begin-1, conductorMax-conductorMin, end-conductorMin)):
                 matrix[y+ offset][conductorMin+1:conductorMax+1] = conductorVoltage
 
-        matrixTmp = np.zeros((sliceLenth, gridLegth + 2), dtype=np.float64)
-        upSlice = np.zeros(gridLegth + 2, dtype=np.float64)
-        downSlice = np.zeros(gridLegth + 2, dtype=np.float64)
+        matrixTmp = np.zeros((PartLenth, gridLegth + 2), dtype=np.float64)
+        upPart = np.zeros(gridLegth + 2, dtype=np.float64)
+        downPart = np.zeros(gridLegth + 2, dtype=np.float64)
         result = np.zeros((gridLegth, gridLegth + 2), dtype=np.float64)
 
         for i in range(iterations):
-            matrix = updateMatrix(comm, rank, numberOfCores, gridLegth, matrix, matrixTmp, upSlice, downSlice, begin, end)
+            matrix = updateMatrix(comm, rank, numberOfCores, gridLegth, matrix, matrixTmp, upPart, downPart, begin, end)
             if begin <conductorMax and end > conductorMin:
                 offset = max(0, conductorMin-begin)
                 for y in range(min(conductorMax-begin, end-begin -1, conductorMax-conductorMin,end-conductorMin)):
                     matrix[y+ offset][conductorMin+1:conductorMax+1] = conductorVoltage
 
-        joinCalculations(comm, rank, numberOfCores, gridLegth, matrix, sliceLenth, result)
+        getResult(comm, rank, numberOfCores, gridLegth, matrix, PartLenth, result)
 
         if rank == 0:
             times += time.time() - start
